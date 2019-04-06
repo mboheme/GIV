f83eac202489738a47c8750d0e54cfbd8306b656
mboheme <fraisemania@hotmail.com>
2019-04-06 12:29:18 +0200

    Update create_database.sql

--- create_database.sql
+++ create_database.sql
@@ -305,7 +305,7 @@ CREATE TABLE droit_groupe(
 )ENGINE=InnoDB;
 
 ALTER TABLE alerte ADD CONSTRAINT FK_alerte_id_vehicule FOREIGN KEY (id_vehicule) REFERENCES vehicule(id);
-ALTER TABLE alerte ADD CONSTRAINT FK_alerte_id_utilisateur FOREIGN KEY (id_utilisateur) REFERENCES utilisateur(id_utilisateur);
+ALTER TABLE alerte ADD CONSTRAINT FK_alerte_id_utilisateur FOREIGN KEY (id_utilisateur) REFERENCES utilisateur(id);
 ALTER TABLE composant ADD CONSTRAINT FK_composant_id_souscategorie FOREIGN KEY (id_souscategorie) REFERENCES souscategorie(id);
 ALTER TABLE modele ADD CONSTRAINT FK_modele_id_marque FOREIGN KEY (id_marque) REFERENCES marque(id);
 ALTER TABLE souscategorie ADD CONSTRAINT FK_souscategorie_id_categorie FOREIGN KEY (id_categorie) REFERENCES categorie(id);
@@ -313,7 +313,7 @@ ALTER TABLE pneu ADD CONSTRAINT FK_pneu_id_composant FOREIGN KEY (id_composant) 
 ALTER TABLE pneu ADD CONSTRAINT FK_pneu_id_vitesse FOREIGN KEY (id_vitesse) REFERENCES vitesse(id);
 ALTER TABLE vehicule ADD CONSTRAINT FK_vehicule_id_modele FOREIGN KEY (id_modele) REFERENCES modele(id);
 ALTER TABLE vehicule ADD CONSTRAINT FK_vehicule_id_carburant FOREIGN KEY (id_carburant) REFERENCES carburant(id);
-ALTER TABLE intervention ADD CONSTRAINT FK_intervention_id_utilisateur FOREIGN KEY (id_utilisateur) REFERENCES utilisateur(id_utilisateur);
+ALTER TABLE intervention ADD CONSTRAINT FK_intervention_id_utilisateur FOREIGN KEY (id_utilisateur) REFERENCES utilisateur(id);
 ALTER TABLE utilisateur ADD CONSTRAINT FK_id FOREIGN KEY (id) REFERENCES groupe(id);
 ALTER TABLE serrage ADD CONSTRAINT FK_serrage_id_intervention FOREIGN KEY (id_intervention) REFERENCES intervention(id);
 ALTER TABLE serrage ADD CONSTRAINT FK_serrage_id_composant FOREIGN KEY (id_composant) REFERENCES composant(id);
@@ -329,7 +329,7 @@ ALTER TABLE niveau ADD CONSTRAINT FK_niveau_id_intervention FOREIGN KEY (id_inte
 ALTER TABLE niveau ADD CONSTRAINT FK_niveau_id_composant FOREIGN KEY (id_composant) REFERENCES composant(id);
 ALTER TABLE compasant_vehicule ADD CONSTRAINT FK_compasant_vehicule_id FOREIGN KEY (id) REFERENCES composant(id);
 ALTER TABLE compasant_vehicule ADD CONSTRAINT FK_compasant_vehicule_id_vehicule FOREIGN KEY (id_vehicule) REFERENCES vehicule(id);
-ALTER TABLE vehicule_utilisateur ADD CONSTRAINT FK_vehicule_utilisateur_id_utilisateur FOREIGN KEY (id_utilisateur) REFERENCES utilisateur(id_utilisateur);
+ALTER TABLE vehicule_utilisateur ADD CONSTRAINT FK_vehicule_utilisateur_id_utilisateur FOREIGN KEY (id_utilisateur) REFERENCES utilisateur(id);
 ALTER TABLE vehicule_utilisateur ADD CONSTRAINT FK_vehicule_utilisateur_id FOREIGN KEY (id) REFERENCES vehicule(id);
 ALTER TABLE droit_groupe ADD CONSTRAINT FK_droit_groupe_id FOREIGN KEY (id) REFERENCES droit(id);
 ALTER TABLE droit_groupe ADD CONSTRAINT FK_droit_groupe_id_groupe FOREIGN KEY (id_groupe) REFERENCES groupe(id);
